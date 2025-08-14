13371622a56503439002bd96c70c9a3212a793db
KONDO <chamsiyak@gmail.com>
2025-08-14 23:26:56 +0200

    Day 23 Exercises

---
+++ day_23/virtual-environments.py
@@ -0,0 +1,17 @@
+#Exercises: Day 23
+#1. Create a project directory with a virtual environment based on the example given above
+
+"""
+C:\Users\admin\CODING\PYTHON2\30daysofpython\flask_project>venv\Scripts\activate
+
+(venv) C:\Users\admin\CODING\PYTHON2\30daysofpython\flask_project>pip freeze
+
+blinker==1.9.0
+click==8.2.1
+colorama==0.4.6
+Flask==3.1.1
+itsdangerous==2.2.0
+Jinja2==3.1.6
+MarkupSafe==3.0.2
+Werkzeug==3.1.3
+"""
