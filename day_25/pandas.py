56c970ee77ddd33d4ac976f6fd143bac8d451508
KONDO <chamsiyak@gmail.com>
2025-08-16 23:49:00 +0200

    Day 25 Exercises

---
+++ day_25/pandas.py
@@ -0,0 +1,19 @@
+#Exercises: Day 25
+#1. Read the hacker_news.csv file from data directory
+import pandas as pd
+
+df = pd.read_csv('hacker_news.csv')
+print(df)
+
+#2. Get the first five rows
+first_five_rows = df.head()
+print(first_five_rows)
+
+#3. Get the last five rows
+last_five_rows = (df.tail())
+print(last_five_rows)
+
+#4. Get the title column as pandas series
+title = df.title()
+
+#5. Count the number of rows and columns
