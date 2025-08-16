ccaecc249871965a5639d8691c39e365a045b983
KONDO <chamsiyak@gmail.com>
2025-08-16 02:20:55 +0200

    Day 24 Exercises

--- day_24/statistics.py
+++ day_24/statistics.py
@@ -168,3 +168,95 @@ print('First column:', first_column)
 print('Second column:', second_column)
 print('Third column: ', third_column)
 print(two_dimension_array)
+
+#Slicing Numpy array
+two_dimension_array = np.array([[1,2,3],[4,5,6], [7,8,9]])
+first_two_rows_and_columns = two_dimension_array[0:2, 0:2]
+print(first_two_rows_and_columns)
+
+#How to reverse the rows and the whole array?
+print(two_dimension_array[::])
+
+#Reverse the row and column positions
+print(two_dimension_array[::-1,::-1])
+
+#Represent missing values ?
+print(two_dimension_array)
+two_dimension_array[1,1] = 55
+two_dimension_array[1,2] =44
+print(two_dimension_array)
+
+#Numpy Zeroes
+# numpy.zeros(shape, dtype=float, order='C')
+numpy_zeroes = np.zeros((3,3),dtype=int,order='C')
+print(numpy_zeroes)
+
+# Numpy Zeroes
+numpy_ones = np.ones((3,3),dtype=int,order='C')
+print(numpy_ones)
+
+twoes = numpy_ones * 2
+print(twoes)
+
+#How to create repeating sequences?
+a = [1,2,3]
+# Repeat whole of 'a' two times
+print('Tile: ', np.tile(a, 2))
+# Repeat each element of 'a' two times
+print('Repeat: ', np.repeat(a, 2))
+
+
+#How to generate random numbers?
+# One random number between [0,1)
+one_random_num = np.random.random(1)
+one_random_in = np.random
+print(one_random_num)
+
+# Random numbers between [0,1) of shape 2,3
+r = np.random.random(size=[2,3])
+print(r)
+print(np.random.choice(['a', 'e', 'i', 'o', 'u'], size=10))
+
+## Random numbers between [0, 1] of shape 2, 2
+rand = np.random.rand(2,2)
+print(rand)
+
+rand2 = np.random.randn(2,2)
+print(rand2)
+
+# Random integers between [0, 10) of shape 2,5
+rand_int = np.random.randint(0, 10, size=[5,3])
+print(rand_int)
+
+#from scipy import stats
+np_normal_dis = np.random.normal(5, 0.5, 1000) 
+np_normal_dis
+## min, max, mean, median, sd
+print('min: ', np.min(np_normal_dis))
+print('max: ', np.max(np_normal_dis))
+print('mean: ', np.mean(np_normal_dis))
+print('median: ', np.median(np_normal_dis))
+#print('mode: ', stats.mode(np_normal_dis))
+print('sd: ', np.std(np_normal_dis))
+
+"""plt.hist(np_normal_dis, color="grey", bins=21)
+plt.show()"""
+
+#inear Algebra
+#1. Dot Product
+## Linear algebra
+### Dot product: product of two arrays
+f = np.array([1,2,3])
+g = np.array([4,5,3])
+### 1*4+2*5 + 3*6
+print(np.dot(f, g))
+
+### Matmul: matruc product of two arrays
+h = [[1,2],[3,4]]
+i = [[5,6],[7,8]]
+### 1*5+2*7 = 19
+np.matmul(h, i)
+
+## Determinant 2*2 matrix
+### 5*8-7*6np.linalg.det(i)
+np.linalg.det(i)
